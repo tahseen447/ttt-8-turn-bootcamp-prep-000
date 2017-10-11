3fd7721e6eeab35f00ef6808fc6b5cf29058842c
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-11 21:57:19 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,53 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+
+def input_to_index(input)
+  user_input = input.to_i
+  user_input-1
+end
+
+def move(array, position, value="X")
+  array[position]=value
+end
+
+
+def valid_move?(board, index)
+  if((index >=0 && index <=8) && !position_taken?(board, index))
+    true
+  else
+    nil
+  end
+end
+
+
+def turn(board)
+  puts "Welcome to Tic Tac Toe!"
+  board =[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+  puts "Where would you like to go?"
+  input = gets.strip
+
+  index = input_to_index(input)
+  move(board, index)
+  display_board(board)
+
+end
+
+def position_taken?(board, index)
+  if(board[index]=="")
+    false
+  elsif(board[index]==" ")
+    false
+ elsif(board[index]==nil)
+  false
+ elsif(board[index]="X" || board[index]=="O")
+  true
+end
+end
